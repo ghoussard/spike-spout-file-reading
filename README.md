783b476eb2dabf1de9f3c8ef1518aa0be50ab8b2
Grégoire Houssard <gregoire.houssard@akeneo.com>
2022-02-01 16:50:29 +0100

    Test merged cells

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spike Spout File Reading
+
+## Tested Cases
+- [x] Merged cells
+- [ ] Hidden cells
+- [ ] Cells decoration
+- [ ] Image inside
+- [ ] Formula inside
+- [ ] Client files ?
---
+++ src/merged_cells.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Akeneo\SpikeSpoutFileReading;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
+
+$filePath = __DIR__ . '/files/merged_cells.xlsx';
+$reader = ReaderEntityFactory::createXLSXReader();
+$reader->open($filePath);
+
+foreach ($reader->getSheetIterator() as $sheet) {
+    print('Sheet: ' . $sheet->getName() . PHP_EOL);
+
+    // here we can select our desired sheet by index or name ($sheet->getName()/$sheet->getIndex())
+    // then we can iterate on rows with an iterator
+    $i = 0;
+    foreach ($sheet->getRowIterator() as $row) {
+        print('Row: ' . $i++ . PHP_EOL);
+        var_dump($row->toArray());
+    }
+}
+
+$reader->close();
